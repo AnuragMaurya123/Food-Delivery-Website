bee261a3d83c49dc75bcfba652464d45d4f35877
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-11-03 15:31:20 +0530

    project done

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectCloudinary()
 //midlleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://food-delivery-website-a3pd.vercel.app'],
+  origin: ['https://food-delivery-website-4dvo.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
