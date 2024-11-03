486caa431a8ef53920ba2b6cd03c1ceb5ad424dc
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-11-03 19:20:31 +0530

    project done

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectCloudinary()
 //midlleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://foods-databases.web.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
