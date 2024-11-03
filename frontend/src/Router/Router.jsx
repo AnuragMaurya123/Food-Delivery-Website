98d068ee94c6ae2f0326d50664aa59a201ee7837
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-11-03 17:13:08 +0530

    project done

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/order',
-        element: <Orders />, 
+        element: <ProtectedRouter><Orders /></ProtectedRouter>, 
       }
       ,
       {
